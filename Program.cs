d421b6ab659cce16cf8d40d8a70db4dba92448ab
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2023-12-09 18:08:53 -0300

    desafio poo

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia("123456789", "Nokia 1", "11111", 128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone("987654321", "Iphone 15", "22222", 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
